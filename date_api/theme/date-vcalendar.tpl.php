4704b7ab8d43b451b90174d1fb60ec4332c7b4a4
Karen Stevenson <karen@elderweb.com>
2011-08-18 07:47:53 -0500

    Issue #742146, Add option to remove X-WR-CALNAME if VEVENT is not a feed.

--- date_api/theme/date-vcalendar.tpl.php
+++ date_api/theme/date-vcalendar.tpl.php
@@ -14,7 +14,9 @@ if (empty($method)) {
 BEGIN:VCALENDAR
 VERSION:2.0
 METHOD:<?php print $method; ?>
-X-WR-CALNAME;VALUE=TEXT:<?php print $calname ?>
+<?php if !empty($calname): ?>
+X-WR-CALNAME;VALUE=TEXT:<?php print $calname . "\r\n"; ?>
+<?php endif; ?>
 PRODID:-//Drupal iCal API//EN
 <?php foreach($events as $event): ?>
 <?php print theme('date_vevent', $event); ?>
